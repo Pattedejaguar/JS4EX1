d65606dcc1f320c36e8af964717e71091213785b
Yannick L <lef.yannick@gmail.com>
2019-04-16 15:17:36 +0200

    fonction qui prend deux nombres en paramètres et qui retourne le résultat de leur addition.

---
+++ main.js
@@ -0,0 +1,7 @@
+function addition(){
+
+let valeur1 = +document.getElementById('nb1').value;
+let valeur2 = +document.getElementById('nb2').value;
+let result = valeur1 + valeur2;
+alert (result)
+}
